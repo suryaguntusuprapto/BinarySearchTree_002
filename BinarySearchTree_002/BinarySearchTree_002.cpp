d05be5f730ddc04d4ba9f609e5efbf1b72be42e9
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-07-02 19:17:16 +0700

    memasukkan method ke tampilan menu

--- BinarySearchTree_002/BinarySearchTree_002.cpp
+++ BinarySearchTree_002/BinarySearchTree_002.cpp
@@ -123,5 +123,39 @@ int main()
 		cout << "4. Perform postorder traversal" << endl;
 		cout << "5. Exit" << endl;
 		cout << "\nEnter your choice (1-5) : ";
+
+		char ch;
+		cin >> ch;
+		cout << endl;
+
+		switch (ch)
+		{
+		case '1': {
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
+		case '2': {
+			obj.inorder(obj.ROOT);
+			break;
+		}
+		case '3': {
+			obj.preorder(obj.ROOT);
+			break;
+		}
+		case '4': {
+			obj.postorder(obj.ROOT);
+			break;
+		}
+		case '5':
+			return 0;
+		default:
+		{
+			cout << "Invalid option" << endl;
+			break;
+		}
+		}
 	}
 }
